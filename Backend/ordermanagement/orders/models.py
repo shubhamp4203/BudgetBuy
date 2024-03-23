bd839994cf9b3fc2fac2762df6161663b8fde897
Shubham Pachchigar <shubhampachchigar0403@gmail.com>
2024-03-23 16:15:34 +0530

    added getInv and addStock

--- Backend/ordermanagement/orders/models.py
+++ Backend/ordermanagement/orders/models.py
@@ -8,16 +8,25 @@ class Order(models.Model):
     user_id = models.BigIntegerField()
     shipping_address = models.CharField(max_length=200, null=True)
     total_value = models.IntegerField(null=True, default=0)
-    payment_method = models.CharField(max_length=50, null=True)  
+    payment_method = models.CharField(max_length=50, null=True) 
+
+    class Meta:
+        db_table = 'order' 
 
 class Order_item(models.Model):
     order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
     product_id = models.BigIntegerField()
     amount = models.IntegerField(null=True)
+    
+    class Meta:
+        db_table = 'order_item'
 
 class Cart(models.Model):
     user_id = models.BigIntegerField(primary_key=True)
     total_value = models.IntegerField(null=True, default=0)
+
+    class Meta:
+        db_table = 'cart'
 
 
 class Cart_item(models.Model):
@@ -25,5 +34,8 @@ class Cart_item(models.Model):
     user_id = models.ForeignKey(Cart, on_delete=models.CASCADE)
     amount = models.IntegerField(null=True)
     product_price = models.IntegerField(null=True)
+
+    class Meta:
+        db_table = 'cart_item'
 
 
