23a35269d019c8c0948591af28fb4b41fb96ea44
Shubham Pachchigar <shubhampachchigar0403@gmail.com>
2024-03-24 16:14:46 +0530

    updated inventory and order api

--- Backend/inventorymanagement/inventory/urls.py
+++ Backend/inventorymanagement/inventory/urls.py
@@ -4,5 +4,7 @@ from inventory import views
 urlpatterns = [
     path('addStock/', views.addStock),
     path('getInv/', views.getInventory),
-    path('checkStock/', views.checkStock)
+    path('checkStock/', views.checkStock),
+    path('updateStock/', views.updateStock),
+    path('editStock/', views.editStock)
 ]
