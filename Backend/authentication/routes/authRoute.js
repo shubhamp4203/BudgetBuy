12703b3604095b2db763dfb5dc51e62146ed5072
Rohanadatia35 <rohan.adatia11@gmail.com>
2024-03-30 00:31:22 +0530

    auth addded

--- Backend/authentication/routes/authRoute.js
+++ Backend/authentication/routes/authRoute.js
@@ -1,11 +1,12 @@
 const {Router}=require("express");
 const authController = require("../controllers/authControllers");
 const router=Router();
+const auth=require("../Middlewares/auth")
 
 //Routes handelling
 router.post("/signup",authController.signup_post)
 router.post("/login",authController.login_post)
-router.put("/update",authController.updateUser_put)
+router.put("/update",auth,authController.updateUser_put)
 router.post("/logout",authController.logout_post)
 
 module.exports=router
