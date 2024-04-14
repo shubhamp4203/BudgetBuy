1c3335301d93c6f16b947106e7116af0f05023b5
Rohanadatia35 <rohan.adatia11@gmail.com>
2024-04-14 23:44:52 +0530

    wishlist

--- Backend/Product_Details/Routes/Routes.js
+++ Backend/Product_Details/Routes/Routes.js
@@ -5,8 +5,10 @@ const multer = require("multer");
 
 const upload = multer({dest: 'uploads/'});
 
-router.post('/insertproduct',upload.single('image'),Controller.insertProduct_post)
-router.post('/getproduct',Controller.getProduct_post)
-router.get('/getAll',Controller.allproducts_get)
+router.post('/insertproduct',upload.single('image'),Controller.insertProduct_post);
+router.post('/getproduct',Controller.getProduct_post);
+router.get('/getAll',Controller.allproducts_get);
+router.post('/wishlist',Controller.wishlist_post);
+router.post('/removeWishlist',Controller.removeWishlist_post);
 
 module.exports=router;
